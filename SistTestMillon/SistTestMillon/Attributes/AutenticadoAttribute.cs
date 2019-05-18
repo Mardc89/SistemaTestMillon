4bb916fcd59a5bd7616d4d7c1bc94dea51fd6703
Mardc89 <mardc0602@gmail.com>
2019-05-17 22:31:27 -0500

    autentificacion...

---
+++ SistTestMillon/SistTestMillon/Attributes/AutenticadoAttribute.cs
@@ -0,0 +1,69 @@
+﻿using Model;
+using Repository;
+using SistTestMillon.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SistTestMillon.Attributes
+{
+    public class AutenticadoAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+
+            if (!SessionHelper.ExistUserInSession())
+            {
+                filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new
+                {
+                    controller = "Account",
+                    action = "Index"
+                }));
+            }
+            else
+            {
+                IRepository Repositorio = new Model.Repository();
+                int idUser = SessionHelper.GetUser();
+                var Usuario = Repositorio.FindEntity<Usuarios>(c => c.IdUsuario == idUser);
+                if (Usuario != null)
+                {
+                    SessionHelper.ActualizarSession(Usuario);
+                }
+            }
+        }
+    }
+
+    public class NoLoginAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+
+            if (SessionHelper.ExistUserInSession())
+            {
+                IRepository Repositorio = new Model.Repository();
+                int idUser = SessionHelper.GetUser();
+                var Usuario = Repositorio.FindEntity<Usuarios>(c => c.IdUsuario == idUser);
+                if (Usuario != null)
+                {
+                    SessionHelper.ActualizarSession(Usuario);
+                    if (Usuario.IdUsuario == 1)
+                    {
+                        filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new
+                        {
+                            controller = "Admin",
+                            action = "Index"
+                        }));
+                    }
+                }
+
+            }
+        }
+    }
+
+
+}
---
+++ SistTestMillon/SistTestMillon/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Model;
+using Repository;
+using SistTestMillon.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SistTestMillon.Controllers
+{
+    [Autenticado]
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            IRepository repository = new Model.Repository();
+            var objResult = repository.FindEntity<Usuarios>(c => true);
+            return View(objResult);
+        }
+    }
+}
